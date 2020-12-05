70e8c8951dc7d6ae97da71fb7f5212d02e185481
Agyey Arya <agyey27@yahoo.co.in>
2020-12-05 21:01:45 +0530

    Establish Connection

--- hack.py
+++ hack.py
@@ -1,1 +1,16 @@
-# write your code here
+import socket
+import sys
+
+# Get hostname, port and message
+args = sys.argv
+hostname, port, message = args[-3:]
+address = (hostname, int(port))
+
+# Create Socket and connect
+with socket.socket() as connection_socket:
+    connection_socket.connect(address)
+    # Send Message
+    connection_socket.send(message.encode())
+    # Get Response
+    response = connection_socket.recv(1024)
+    print(response.decode())
